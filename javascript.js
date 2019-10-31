1570fb16aba8b0c624fce1633af3a33efd713fd3
Toni Allende <decimetoni@hotmail.com>
2019-10-31 13:40:50 -0300

    cv terminado

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     console.log('arrancamo');
+//     $('.der').mouseover(function(){
+//         this.animate({left: "500px", height: "200px", width: "200px"},5000);
+//     });
+// });
+
+
+
+
+
+
